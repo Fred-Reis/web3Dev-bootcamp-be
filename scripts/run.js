9f3dd0651dbc62e064baf175b6f817acea0eb512
Fred-Reis <fredgost@hotmail.com>
2022-05-10 00:34:34 -0300

    new functions added

--- scripts/run.js
+++ scripts/run.js
@@ -1,24 +1,27 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
 
   console.log("wave contract deployed to: ", waveContract.address);
-  console.log("wave contract deployed by: ", owner.address);
+  // console.log("wave contract deployed by: ", owner.address);
 
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
+  console.log("total waves: ", waveCount.toNumber());
 
-  let waveTxn = await waveContract.wave();
+  let waveTxn = await waveContract.wave("Some message!");
   await waveTxn.wait();
 
-  waveCount = await waveContract.getTotalWaves();
+  // waveCount = await waveContract.getTotalWaves();
 
-  waveTxn = await waveContract.connect(randomPerson).wave();
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
   await waveTxn.wait();
 
-  waveCount = await waveContract.getTotalWaves();
+  // waveCount = await waveContract.getTotalWaves();
+  let allWaves = await waveContract.getAllWaves();
+  console.log("all waves: ", allWaves);
 };
 
 const runMain = async () => {
